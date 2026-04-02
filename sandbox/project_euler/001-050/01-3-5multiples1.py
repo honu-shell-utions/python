91f06ed80a59d5e731770ac5b1a2606ce333356d
James McCleery <jim.mccleery@gmail.com>
2026-04-02 12:06:05 -1000

    Update of problem 01 in Project Euler

--- sandbox/project_euler/001-050/01-3-5multiples1.py
+++ sandbox/project_euler/001-050/01-3-5multiples1.py
@@ -1,13 +1,21 @@
-#If we list all the natural numbers below 10 that are multiples of 3 or 5,
-#we get 3, 5, 6 and 9. The sum of these multiples is 23.
-#Find the sum of all the multiples of 3 or 5 below 1000.
+# Jim McCleery
+# April 2, 2026
+# Kailua-Kona, HI
+#
+# If we list all the natural numbers below 10 that are multiples of 3 or 5,
+# we get 3, 5, 6, and 9. The sum of these multiples is 23.
+# Find the sum of all the multiples of 3 or 5 below 1000.
 
-MAX = 1000
+LIMIT = 1000
 total = 0
 
-for i in range(1,MAX):
+# Check every number from 1 up to (but not including) 1000.
+for i in range(1, LIMIT):
+    # Add the number if it is divisible by 3 or by 5.
     if i % 3 == 0 or i % 5 == 0:
         total += i
 
+# Display the final sum.
 print(total)
-#solution: 233168
+
+# Solution: 233168
